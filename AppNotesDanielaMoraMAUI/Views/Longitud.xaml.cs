e8d5fbd5d2eb01bbdec547808cbb24efc2490d31
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-11 12:25:31 -0500

    Cambios finales

--- AppNotesDanielaMoraMAUI/Views/Longitud.xaml.cs
+++ AppNotesDanielaMoraMAUI/Views/Longitud.xaml.cs
@@ -19,6 +19,12 @@ namespace AppNotesDanielaMoraMAUI.Views
             numerosDc.Text = CalculoDC(Dcinput);
         }
 
+        private void CalcularMMD(object sender, EventArgs e)
+        {
+            string Mminput = cantidad_DM.Text;
+            numerosMm.Text = CalculoMM(Mminput);
+        }
+
         private string CalculoKM(string Kminput)
         {
             if (double.TryParse(Kminput, out double kilometros))
@@ -37,6 +43,19 @@ namespace AppNotesDanielaMoraMAUI.Views
             if (double.TryParse(Dcinput, out double decimetros))
             {
                 double metros = decimetros * 100;
+                return metros.ToString();
+            }
+            else
+            {
+                return "Valor mal añadido";
+            }
+        }
+
+        private string CalculoMM(string Mminput)
+        {
+            if (double.TryParse(Mminput, out double milimetros))
+            {
+                double metros = milimetros * 1000;
                 return metros.ToString();
             }
             else
